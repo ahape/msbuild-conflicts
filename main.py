5c5fc3f6855478c22fb3893018f03623a81c48ca
Alan Hape <ahape@brightmetrics.com>
2022-08-21 16:40:31 -0700

    Clean up

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ import xml.etree.ElementTree as ET
 cache_file = "cache.json"
 token_rx = re.compile(r"\((.*)\)\s+")
 assemblies = {}
-root = None
-
-# TODO: Normalize as unix paths
-# directory should NOT end with "/"
 
 def get_dll_text(dll_path):
   return os.popen(f"ildasm /text {dll_path}").read()
@@ -26,16 +22,16 @@ def update_assemblies(refs, module):
   for name, asm in refs.items():
     if name in assemblies:
       orig = assemblies[name]
-      if not orig["version"] and asm["version"]:
+      if orig["version"] == "0.0.0.0" and asm["version"] != "0.0.0.0": # Take strong over weak
         orig["version"] = asm["version"]
-      if not orig["publickeytoken"] and asm["publickeytoken"]:
+      if not orig["publickeytoken"] and asm["publickeytoken"]: # Take strong over weak
         orig["publickeytoken"] = asm["publickeytoken"]
     else:
       assemblies[name] = asm
     if name != module:
-      this = assemblies[name]
+      orig = assemblies[name]
       # Want to just default nulls to something
-      this["refs"].append({ "name": module, "version": asm["version"] or this["version"] })
+      orig["refs"].append({ "name": module, "version": asm["version"] })
   if module in assemblies:
     assemblies[module]["deps"] += [{ 
       "name": v["name"],
@@ -45,8 +41,7 @@ def update_assemblies(refs, module):
 def get_references_from_dll(dll_path):
   dll = get_dll_text(dll_path)
   refs = {}
-  latest = None
-  module = None
+  latest, module = None, None
 
   for line in dll.splitlines():
     line = line.strip()
@@ -81,44 +76,6 @@ def cache_assemblies():
   with open(cache_file, "w") as f:
     f.write(json.dumps(assemblies, indent=2))
 
-def optimize_assemblies():
-  singles = {}
-  copy = assemblies.copy()
-  copy.pop("mscorlib")
-  copy.pop("netstandard")
-  copy.pop("System")
-  copy.pop("System.Core")
-
-  # Get rid of things not referenced
-  for k, v in copy.copy().items():
-    if len(v["refs"]) == 0 and k != root:
-      copy.pop(k)
-  for asm in copy.values():
-    new_refs = []
-    for ref in asm["refs"]:
-      if ref["name"] in copy:
-        new_refs.append(ref)
-    asm["refs"] = new_refs
-    new_deps = []
-    for ref in asm["deps"]:
-      if ref["name"] in copy:
-        new_deps.append(ref)
-    asm["deps"] = new_deps
-
-  # Squash (group) single references
-  for k, v in copy.copy().items():
-    if len(v["refs"]) == 1 and len(v["deps"]) == 0:
-      name = v["refs"][0]["name"]
-      if name not in singles:
-        singles[name] = v.copy()
-      else:
-        singles[name]["name"] += "\n" + k
-      copy.pop(k)
-  for asm in singles.values():
-    copy[asm["name"]] = asm
-
-  return copy
-
 def parse_config(file):
   root = ET.parse(file).getroot()
   xmlns = "{urn:schemas-microsoft-com:asm.v1}"
@@ -143,11 +100,6 @@ def dump_config_data(directory):
   with open("out/configs.json", "w") as f:
     f.write(json.dumps(configs, indent=2))
 
-def find(arr, cb):
-  for e in arr:
-    if cb(e):
-      return e
-
 def create_graph():
   g = GV.Digraph(
     filename="Diagram",
@@ -155,9 +107,6 @@ def create_graph():
     #engine="neato",
     format="svg")
   
-  """
-  for asm in optimize_assemblies().values():
-  """
   for asm in assemblies.values():
     for ref in asm["refs"]:
       g.edge(ref["name"], asm["name"], label=ref["version"])
