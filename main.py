73381b9a0c7d9506aa38ef974be465c79de9814e
Alan Hape <ahape@brightmetrics.com>
2022-08-22 20:25:01 -0700

    Done for now

--- main.py
+++ main.py
@@ -1,142 +1,45 @@
-import sys, os, json, re
+import sys, re
 import graphviz as GV
-import xml.etree.ElementTree as ET
 
-cache_file = "cache.json"
-token_rx = re.compile(r"\((.*)\)\s+")
-conflict_asms_rx = re.compile(r"'(.*?)'")
-conflict_proj_rx = re.compile(r'Done building target "ImplicitlyExpandNETStandardFacades" in project "(.*)\.csproj"\.')
 ref_which_dep_rx = re.compile(r'References which depend on "(.*?)"')
-dep_on_rx = re.compile(r"^         \S")
-include_item_rx = re.compile(r"^             \S")
+conflict_dep_rx = re.compile(r"^         \S")
+conflict_dep_dep_rx = re.compile(r"^             \S")
 dll_rx = re.compile(r"\\([^\\]+)\.(?:exe|dll)")
 csproj_rx = re.compile(r"\\([^\\]+)\.csproj")
-assemblies = {}
 
 class Node():
-  def __init__(self, name, conflict=False):
+  def __init__(self, name, is_conflict=False):
     self.name = name
-    self.conflict = conflict
+    self.is_conflict = is_conflict
+    self.is_root_proj = False
     self.references = set()
-
   def __hash__(self):
     return id(self.name)
 
-class Ref():
+class Ref(Node):
   def __init__(self, node, version="", is_primary=False):
-    self.node = node
+    super().__init__(node.name)
     self.version = version
+    self.is_root_proj = node.is_root_proj
     self.is_primary = is_primary
-
   def __hash__(self):
-    return id(self.node.name + self.version)
+    return super.__hash__(self)
 
-def get_dll_text(dll_path):
-  return os.popen(f"ildasm /text {dll_path}").read()
-
-def extract_token(line):
-  return token_rx.search(line)[1].replace(" ", "").lower()
-
-def extract_version(line):
-  return line.split(" ")[-1].replace(":", ".")
-
-def extract_module(line):
-  return line.split(" ")[-1].replace(".exe", "").replace(".dll", "")
-
-def update_assemblies(refs, module):
-  for name, asm in refs.items():
-    if name in assemblies:
-      orig = assemblies[name]
-      if orig["version"] == "0.0.0.0" and asm["version"] != "0.0.0.0": # Take strong over weak
-        orig["version"] = asm["version"]
-      if not orig["publickeytoken"] and asm["publickeytoken"]: # Take strong over weak
-        orig["publickeytoken"] = asm["publickeytoken"]
-    else:
-      assemblies[name] = asm
-    if name != module:
-      orig = assemblies[name]
-      # Want to just default nulls to something
-      orig["refs"].append({ "name": module, "version": asm["version"] })
-  if module in assemblies:
-    assemblies[module]["deps"] += [{ 
-      "name": v["name"],
-      "version": v["version"]
-    } for v in refs.values() if v["name"] != module]
-
-def get_references_from_dll(dll_path):
-  dll = get_dll_text(dll_path)
-  refs = {}
-  latest, module = None, None
-  for line in dll.splitlines():
-    line = line.strip()
-    if line.startswith(".assembly "):
-      name = line.split(" ")[-1]
-      latest = { "refs": [], "deps": [], "version": "0.0.0.0", "publickeytoken": None, "name": name }
-      refs[name] = latest
-    elif latest is not None:
-      if line.startswith(".publickeytoken "):
-        latest["publickeytoken"] = extract_token(line)
-      elif line.startswith(".ver "):
-        latest["version"] = extract_version(line)
-      elif line.startswith("}"):
-        latest = None
-    if line.startswith(".module "):
-      module = extract_module(line)
-  update_assemblies(refs, module)
-
-def load_assemblies(directory):
-  for root, _, files in os.walk(directory):
-    for file in files:
-      if file.endswith(".exe") or file.endswith(".dll"):
-        get_references_from_dll(os.path.join(root, file))
-
-def load_assemblies_from_cache():
-  global assemblies
-  with open(cache_file) as f:
-    assemblies = json.loads(f.read())
-
-def cache_assemblies():
-  with open(cache_file, "w") as f:
-    f.write(json.dumps(assemblies, indent=2))
-
-def parse_config(file):
-  root = ET.parse(file).getroot()
-  xmlns = "{urn:schemas-microsoft-com:asm.v1}"
-  bindings = []
-  for el in root.findall(f".//{xmlns}dependentAssembly"):
-    identity = el.find(f".//{xmlns}assemblyIdentity")
-    redirect = el.find(f".//{xmlns}bindingRedirect")
-    if ET.iselement(identity) and ET.iselement(redirect):
-      bindings.append({
-        "name": identity.get("name"),
-        "oldVersion": redirect.get("oldVersion"),
-        "newVersion": redirect.get("newVersion"),
-      })
-  return bindings
-
-def dump_config_data(directory):
-  configs = {}
-  for root, _, files in os.walk(directory):
-    for file in files:
-      if file.endswith(".config"):
-        configs[file] = parse_config(os.path.join(root, file))
-  with open("out/configs.json", "w") as f:
-    f.write(json.dumps(configs, indent=2))
+class Assembly():
+  def __init__(self):
+    self.name = None
+    self.version = None
 
 def parse_fusion_name(fusion):
   parts = fusion.split(", ")
-  retval = {}
+  asm = Assembly()
   name = parts[0]
   if ":" in name:
     name = name[name.index(":") + 1:]
-  name = name.replace(".dll", "").replace(".exe", "").replace("'", "")
-  parts[0] = name
-  retval["name"] = parts[0]
+  asm.name = name.replace(".dll", "").replace(".exe", "").replace("'", "")
   if len(parts) > 1:
-    retval["version"] = parts[1].replace("Version=", "")
-  if len(parts) > 3:
-    retval["publicKeyToken"] = parts[3].replace("PublicKeyToken=", "")
-  return retval
+    asm.version = parts[1].replace("Version=", "")
+  return asm
 
 def parse_references_which_depend_on(line):
   fusion_name = ref_which_dep_rx.search(line)[1]
@@ -150,7 +53,7 @@ def find(arr, cb):
 def parse_assembly_name(line):
   if match := dll_rx.search(line):
     return match[1]
-  return parse_fusion_name(line.strip())["name"]
+  return parse_fusion_name(line.strip()).name
 
 def get_node(nodes, name):
   if node := find(nodes, lambda x: x.name == name):
@@ -166,48 +69,51 @@ def parse_project_name(line):
   if match:
     return match[1]
 
-# Partial name of conflict instead of number
-def parse_build_output(file_path, max_conflicts):
+def trim_msberr_line(line):
+  index = line.index("MSB3277:") + len("MSB3277:")
+  return line[index:]
+
+def strip_proj_portion(line):
+  return line[:line.index(" [")]
+
+def parse_build_output(file_path):
   nodes = []
   ref_num = 0
-  root, ref, child, version = None, None, None, None
+  root_proj, conflict, ref, version = None, None, None, None
+  last_proj = None
   with open(file_path) as f:
     for line in f.readlines():
-      if not root and line.startswith("Project "):
+      if not root_proj and line.startswith("Project "):
         name = csproj_rx.search(line)[1]
-        root = get_node(nodes, name)
+        root_proj = get_node(nodes, name)
+        root_proj.is_root_proj = True
       if "MSB3277:" in line:
-        index = line.index("MSB3277:") + len("MSB3277:")
-        line = line[index:] # Start at the beginning of the warning message
+        line = trim_msberr_line(line)
+        proj = parse_project_name(line)
+        line = strip_proj_portion(line)
         if line.startswith(" Found conflicts between different versions of"):
           ref_num = 0
-        elif line.startswith("     References which depend on"):
-          proj = parse_project_name(line)
+        elif line.startswith("     References which depend on"): # Conflict start
           ref_num += 1
-          child = None
+          ref = None
           asm = parse_references_which_depend_on(line)
-          version = asm["version"]
-          ref = get_node(nodes, asm["name"])
-          ref.conflict = True
-        elif ref and dep_on_rx.search(line):
-          proj = parse_project_name(line)
+          version = asm.version
+          conflict = get_node(nodes, asm.name)
+          conflict.is_conflict = True
+        elif conflict and conflict_dep_rx.search(line): # Conflict <- Dep1 start
           name = parse_assembly_name(line)
-          child = get_node(nodes, name)
-          ref.references.add(Ref(child, version, ref_num == 1))
-        elif child and include_item_rx.search(line):
-          proj = parse_project_name(line)
-          line = line[:line.index(" [")]
+          ref = get_node(nodes, name)
+          conflict.references.add(Ref(ref, version, ref_num == 1))
+        elif ref and conflict_dep_dep_rx.search(line): # Conflict <- Dep1 <- Dep2 start
           name = parse_assembly_name(line)
-          grandchild = get_node(nodes, name)
-          child.references.add(Ref(grandchild, version, ref_num == 1))
-          grandchild.references.add(Ref(get_node(nodes, proj), version, ref_num == 1))
-      elif ref and isinstance(max_conflicts, int):
-        break
+          refref = get_node(nodes, name)
+          ref.references.add(Ref(refref, version, ref_num == 1))
+          refref.references.add(Ref(get_node(nodes, proj), version, ref_num == 1))
     else:
-      ref = child = version = None
+      conflict = ref = version = None
   for node in nodes:
-    if len(node.references) == 0 and node.name != root.name:
-      node.references.add(Ref(root))
+    if node.references and node.name != root_proj.name:
+      node.references.add(Ref(root_proj))
   return nodes
 
 def create_graph_simple(nodes):
@@ -218,13 +124,15 @@ def create_graph_simple(nodes):
   combos = set()
   for node in nodes:
     for ref in node.references:
-      a = ref.node.name
+      a = ref.name
       b = node.name
       if a != b and ((a,b) not in combos or (b,a) not in combos):
         combos.add((a,b))
         combos.add((b,a))
-        if node.conflict:
+        if node.is_conflict:
           g.node(b, style="filled", fillcolor="salmon")
+        if ref.is_root_proj:
+          g.node(a, style="filled", fillcolor="aquamarine")
         if ref.version:
           g.edge(a, b, label=ref.version, color="blue" if ref.is_primary else "black")
         else:
@@ -233,51 +141,7 @@ def create_graph_simple(nodes):
   g.attr(splines="true")
   g.view()
 
-def create_graph_complex():
-  g = GV.Digraph(
-    filename="Diagram",
-    directory="out",
-    format="svg")
-  for asm in assemblies.values():
-    for ref in asm["refs"]:
-      g.edge(ref["name"], asm["name"])
-  g.view()
-
-def try_parse_int(x):
-  try:
-    return int(x)
-  except:
-    return None
-
 if __name__ == "__main__":
-  """
-  if len(sys.argv) < 2:
-    print("Need to supply out directory")
-    raise SystemExit
-
-  if len(sys.argv) == 3:
-    build_output_file = sys.argv[-1]
-    directory = sys.argv[-2]
-  else:
-    build_output_file = "build.txt"
-    directory = sys.argv[-1]
-  """
   build_output_file = sys.argv[-1]
-  if max_conflicts := try_parse_int(build_output_file):
-    build_output_file = sys.argv[-2]
-  nodes = parse_build_output(build_output_file, max_conflicts)
+  nodes = parse_build_output(build_output_file)
   create_graph_simple(nodes)
-
-  """
-  print(f"Running for directory: {directory}")
-  if not os.path.exists(cache_file):
-    print(f"No cache file found. Disassembling all dlls in {directory}")
-    load_assemblies(directory)
-    cache_assemblies()
-  else:
-    print("Using cached assembly data")
-    load_assemblies_from_cache()
-
-  dump_config_data(directory)
-  create_graph_complex()
-  """
---
+++ old.py
@@ -0,0 +1,139 @@
+import sys, os, json, re
+import graphviz as GV
+import xml.etree.ElementTree as ET
+
+cache_file = "cache.json"
+token_rx = re.compile(r"\((.*)\)\s+")
+conflict_asms_rx = re.compile(r"'(.*?)'")
+conflict_proj_rx = re.compile(r'Done building target "ImplicitlyExpandNETStandardFacades" in project "(.*)\.csproj"\.')
+assemblies = {}
+
+def create_graph_complex():
+  g = GV.Digraph(
+    filename="Diagram",
+    directory="out",
+    format="svg")
+  for asm in assemblies.values():
+    for ref in asm["refs"]:
+      g.edge(ref["name"], asm["name"])
+  g.view()
+
+def extract_version(line):
+  return line.split(" ")[-1].replace(":", ".")
+
+def extract_module(line):
+  return line.split(" ")[-1].replace(".exe", "").replace(".dll", "")
+
+def update_assemblies(refs, module):
+  for name, asm in refs.items():
+    if name in assemblies:
+      orig = assemblies[name]
+      if orig["version"] == "0.0.0.0" and asm["version"] != "0.0.0.0": # Take strong over weak
+        orig["version"] = asm["version"]
+      if not orig["publickeytoken"] and asm["publickeytoken"]: # Take strong over weak
+        orig["publickeytoken"] = asm["publickeytoken"]
+    else:
+      assemblies[name] = asm
+    if name != module:
+      orig = assemblies[name]
+      # Want to just default nulls to something
+      orig["refs"].append({ "name": module, "version": asm["version"] })
+  if module in assemblies:
+    assemblies[module]["deps"] += [{ 
+      "name": v["name"],
+      "version": v["version"]
+    } for v in refs.values() if v["name"] != module]
+
+def get_references_from_dll(dll_path):
+  dll = get_dll_text(dll_path)
+  refs = {}
+  latest, module = None, None
+  for line in dll.splitlines():
+    line = line.strip()
+    if line.startswith(".assembly "):
+      name = line.split(" ")[-1]
+      latest = { "refs": [], "deps": [], "version": "0.0.0.0", "publickeytoken": None, "name": name }
+      refs[name] = latest
+    elif latest is not None:
+      if line.startswith(".publickeytoken "):
+        latest["publickeytoken"] = extract_token(line)
+      elif line.startswith(".ver "):
+        latest["version"] = extract_version(line)
+      elif line.startswith("}"):
+        latest = None
+    if line.startswith(".module "):
+      module = extract_module(line)
+  update_assemblies(refs, module)
+
+def get_dll_text(dll_path):
+  return os.popen(f"ildasm /text {dll_path}").read()
+
+def extract_token(line):
+  return token_rx.search(line)[1].replace(" ", "").lower()
+
+def load_assemblies(directory):
+  for root, _, files in os.walk(directory):
+    for file in files:
+      if file.endswith(".exe") or file.endswith(".dll"):
+        get_references_from_dll(os.path.join(root, file))
+
+def load_assemblies_from_cache():
+  global assemblies
+  with open(cache_file) as f:
+    assemblies = json.loads(f.read())
+
+def cache_assemblies():
+  with open(cache_file, "w") as f:
+    f.write(json.dumps(assemblies, indent=2))
+
+def parse_config(file):
+  root = ET.parse(file).getroot()
+  xmlns = "{urn:schemas-microsoft-com:asm.v1}"
+  bindings = []
+  for el in root.findall(f".//{xmlns}dependentAssembly"):
+    identity = el.find(f".//{xmlns}assemblyIdentity")
+    redirect = el.find(f".//{xmlns}bindingRedirect")
+    if ET.iselement(identity) and ET.iselement(redirect):
+      bindings.append({
+        "name": identity.get("name"),
+        "oldVersion": redirect.get("oldVersion"),
+        "newVersion": redirect.get("newVersion"),
+      })
+  return bindings
+
+def dump_config_data(directory):
+  configs = {}
+  for root, _, files in os.walk(directory):
+    for file in files:
+      if file.endswith(".config"):
+        configs[file] = parse_config(os.path.join(root, file))
+  with open("out/configs.json", "w") as f:
+    f.write(json.dumps(configs, indent=2))
+
+if __name__ == "__main__":
+  """
+  if len(sys.argv) < 2:
+    print("Need to supply out directory")
+    raise SystemExit
+
+  if len(sys.argv) == 3:
+    build_output_file = sys.argv[-1]
+    directory = sys.argv[-2]
+  else:
+    build_output_file = "build.txt"
+    directory = sys.argv[-1]
+  """
+
+  """
+  print(f"Running for directory: {directory}")
+  if not os.path.exists(cache_file):
+    print(f"No cache file found. Disassembling all dlls in {directory}")
+    load_assemblies(directory)
+    cache_assemblies()
+  else:
+    print("Using cached assembly data")
+    load_assemblies_from_cache()
+
+  dump_config_data(directory)
+  create_graph_complex()
+  """
